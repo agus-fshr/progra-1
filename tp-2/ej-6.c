05d7303bcacb7a53e6c084b29b37e0f46e417043
juan-sb <sbruzzi.juan@outlook.com>
2021-03-17 15:08:15 -0300

    Fix comentarios y tabulación tp2

--- tp-2/ej-6.c
+++ tp-2/ej-6.c
@@ -1,35 +1,42 @@
+/*
+    TP2 - Ejercicio 6 - Grupo 7 (FISHER, MARTINEZ TANOIRA, SBRUZZI, OMS)
+    Consigna:
+        Escribir un programa que imprima en pantalla si un número es 
+		primo o no (un número natural es primo si posee exactamente dos divisores).
+	
+	Nota 1:
+		Si un numero no es primo, entonces tiene algun divisor entre 2 y su raiz cuadrada. 
+		A partir del 4, la raíz cuadrada de todo número es menor a su mitad 
+		(y en el caso de 2 y 3, se cubren los casos usando la mitad + 1). 
+		Esto permite que, en el caso de que el numero sea primo, el programa no chequee valores innecesariamente.
+
+		Ref: https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
+		Adicionalmente consultamos la validez de esto con un ayudante de catedra.
+*/
+
 #include <stdio.h>
 
 int main(void){
 
-        int nro = 997; //nro a chequear si es primo
-        int primo = 1; //"booleano" de primo o no
-        int i; //contador
+	int numero = 997; 		//nro a analizar
+	int primo = 1; 			//"booleano" de primo o no
+	int i; 					//contador
 
-	if(nro>=1){ //chequea si el nro ingresado puede ser primo
+	if(numero >= 1){ 						//chequea si el nro ingresado cumpla las condiciones mínimas para ser primo
+		for (i = 2; i < numero/2+1; i++){ 	//se verifican los nros desde 2 hasta la mitad del num, esto se basa en la idea de la raiz cuadrada (nota 1)
+			if (numero % i == 0){			//si el resto de dividir nro por otro numero es 0, 
+				primo = 0;					//	entonces ese es divisor distinto de nro y 1, entonces no es primo
+				break; 						//sale del loop, para evitar calculo innecesario (especialmente con no primos de gran valor)
+			}
+		}
+		if (primo)
+			printf("El numero %d es primo.\n", numero);
+		else
+			printf("El numero %d no es primo.\n", numero);
+	} else
+		printf("El numero solicitado no es natural, por ende, no puede ser primo.\n"); //por definición de numeros primos
 
-        	for (i=2; i<nro/2+1; i++){ //se chequean los nros desde 2 hasta la mitad de nro, esto se basa en la idea de la raiz cuadrada (mas sobre esto abajo)
-                	if (nro%i==0){ //si el resto de dividir nro por otro numero es 0, entonces ese es divisor distinto de nro y 1, entonces no es primo
-				primo = 0;
-				break; //sale del loop, para evitar calculo innecesario
-                       		}
-        	}
+	return 0;
+}
 
-        	if (primo == 1)
-                	printf("El nro %d es primo\n", nro);
-        	else
-                	printf("El nro %d no es primo\n", nro);
-	}
-	else
-		printf("El numero solicitado no es natural, por ende, no puede ser primo.\n");
 
-        return 0;
-        }
-
-/*
-Si un nro no es primo, entonces tiene algun divisor entre 2 y su raiz cuadrada, a partir del 4, todos los nros tienen su raiz cuadrada, menor
-a su mitad. Esta particion la hacemos para evitar que, en el caso de que nro sea primo, el programa no chequee valores innecesariamente.
-
-https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
-Tambien, consultamos la validez de esto con un ayudante de catedra.
-*/
--- tp-2/ej-7.c
+++ tp-2/ej-7.c
@@ -1,20 +1,35 @@
+/*
+    TP2 - Ejercicio 7 - Grupo 7 (FISHER, MARTINEZ TANOIRA, SBRUZZI, OMS)
+    Consigna:
+        Dadas las variables m (cantidad de filas) y n (cantidad de 
+			columnas), imprimir en pantalla una matriz de números crecientes de izquierda a 
+			derecha y de arriba hacia abajo. Ejemplo con m=5, n=4:
+			1 2 3 4
+			2 3 4 5
+			3 4 5 6
+			4 5 6 7
+			5 6 7 8
+
+	Nota 1:
+		Mientras se produzcan nros de un digito, dejamos el codigo con espacios entre los elementos de las filas. 
+		Para nros mas grandes, \t asegura que el espaciado entre columnas sea uniforme.
+*/
+
 #include <stdio.h>
 
 int main(void){
 
-	int m = 5; //nro de filas
-	int n = 5; //nro de columnas
-	int col, fil; //contadores
+	int m = 5; 		//num de filas
+	int n = 3; 		//num de columnas
+	int col, fil; 	//contadores
 
-	for(fil=0; fil<m; fil++){ //este for controla la fila en la cual se escribe, incrementando de a 1, hasta llegar al m filas
+	for(fil = 0; fil < m; fil++){ 		//for que controla la fila actual, incrementando de a 1, hasta llegar al m filas
 
-		for(col=1; col<=n; col++) //este for controla el texto dentro de cada fila, es decir, las columnas
-			printf("%d  ", col+fil); //hace col+fil para escribir con el incremento que le corresponde a cada fila
+		for(col = 1; col <= n; col++) 	//for que controla el texto dentro de cada fila: las columnas
+			printf("%d  ", col+fil); 	//hace col+fil para escribir con el incremento(offset) que le corresponde a cada fila
 
-		printf("\n"); //pasa a la siguiente fila
+		printf("\n"); 					//pasa a la siguiente fila
 	}
 
 	return 0;
 }
-/*Mientras se haga con nros de un digito, dejamos el codigo con espacios entre los elementos de las filas, aunque para nros mas grandes,
-\t asegura que el espaciado entre columnas sea uniforme.*/
