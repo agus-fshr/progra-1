aecba1d5daacde4e2b9eb57e6189e9ebe20f43c0
Agustin Fisher <agustin.fshr@gmail.com>
2021-04-02 00:33:29 -0300

    lector de mapas terminado

--- 4-tp/conway/number_input.c
+++ 4-tp/conway/number_input.c
@@ -7,24 +7,25 @@ int main(){
     int M[8][8];
     char c;
     int cell_state;
-    int row_counter, col_counter;
+    int row_counter = 0, col_counter = 0;
 
     while((c = getchar()) != EOF) {
         switch (c){
             case(' '):{
                 cell_state = DEAD;
                 M[row_counter][col_counter] = cell_state;
-                col_counter++;
+                ++col_counter;
                 break;
             }
             case('*'):{
                 cell_state = ALIVE;
                 M[row_counter][col_counter] = cell_state;
-                col_counter++;
+                ++col_counter;
                 break;
             }
             case('\n'):{
                 row_counter++;
+                col_counter = 0;
                 break;
             }
             default:{
@@ -34,12 +35,19 @@ int main(){
         // M[row_counter][col_counter] = cell_state;
     }
 
-    for (int i=0; i<=row_counter; i++){
-        for(int n=0; n<=col_counter; n++){
-            printf("%d  ", M[i][n]);
+    // for (int i=0; i<=row_counter; i++){
+        // for(int n=0; n<=col_counter; n++){
+            // printf("%d  ", M[i][n]);
+        // }
+        // printf("\n");
+    // }
+    for(int x = 0 ; x < row_counter ; x++) {
+            printf(" ");
+            for(int y = 0 ; y < col_counter ; y++){
+                printf("%d ", M[x][y]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
 
     return 0;
 }
