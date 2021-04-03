21a99372617151684923cf5f24c7aa333fbfa50d
Juan Francisco Sbruzzi <sbruzzi.juan@outlook.com>
2021-04-03 13:58:50 -0300

    Debugging 5mock

--- 4-tp/5mock.c
+++ 4-tp/5mock.c
@@ -30,46 +30,64 @@ unsigned int world[WORLD_HEIGHT][WORLD_WIDTH];
 unsigned int total_generations;
 
 int main(void){
-    printf("Arranque.\n");
+    printf("Arranque\n");
     char c;
 
     initialize_world();
-    // acá va a haber algo que 
-    /*
-    while((c = getchar()) != 'q') {
-        unsigned int next_generations = read_num();
-        total_generations += next_generations;
+    print_world();
 
+    while((c = getchar()) != 'q') {
+        unsigned int next_generations = 1;
+        printf("\n Num leido: %d\n", next_generations);
+        total_generations += next_generations;
         int i;
         for(i = 0; i < next_generations; i++) {
             advance_generation();
             // acá algo de printeo lindo quizás 
             // podríamos poner el núm de generaciones pasadas, x ej.
+            printf("\n\nSiguiente generación: \n\n");
             print_world();
-            clear_screen();
+            //clear_screen();
         }
-
     }
-    */
 
     printf("A ksita");
 
     return 0;
 }
-
+/*
+una celula viva con exactamente dos o tres vecinos sobrevive;
+una celula muerta con exactamente tres vecinos obtiene vida nuevamente.
+*/
 void advance_generation(void) {
-    int i_col, i_fil;
-    // next_gen
+    int i_col, i_fil, i;
+    unsigned int next_world[WORLD_HEIGHT][WORLD_WIDTH];
     for(i_col = 0; i_col < get_world_width(); i_col++) {
         for(i_fil = 0; i_fil < get_world_height(); i_fil++) {
-            int adjacent_living = read_adjacent_cells(i_col, i_fil);
-            // if(tal condicion)
-            // else if(tal condicion)
-            // etc etc
-            // esto lo tiene que guardar en next_gen
+            int adjacent_living = read_adjacent_cells(i_fil, i_col);
+            int cell_state = get_cell_state(i_fil, i_col);
+            int next_state = DEAD;
+            if(cell_state == ALIVE) {
+                if(adjacent_living == 2 || adjacent_living == 3) {
+                    next_state = ALIVE;
+                }
+            }
+            else {
+                if(adjacent_living == 3) {
+                    next_state = ALIVE;
+                }
+            }
+            next_world[i_fil][i_col] = next_state;
         }
     }
-    // acá guardar next gen en el mundo actual
+    
+    // copiar matriz a world
+    for(i = 0; i < WORLD_HEIGHT; i++) {
+        for(int j = 0; j < WORLD_WIDTH; j++) {
+            world[i][j] = next_world[i][j];
+        }
+    }
+
 }
 unsigned int read_num(void){
     char c;
@@ -118,13 +136,15 @@ void world_from_file() {
 unsigned int read_adjacent_cells(int x, int y) {
     int alive_neighbours = 0;
     int n, m;
-    int out_of_map_rows = (m == -1) || (m == get_world_height());
-    int out_of_map_cols = (n == -1) || (n == get_world_height());
-
+    int out_of_map_rows;
+    int out_of_map_cols;
+    
     for(int n = x-1; n <= x+1; n++){
-        if(!out_of_map_cols){
+        out_of_map_rows = (n == -1) || (n == get_world_height());
+        if(out_of_map_rows == 0){
             for(int m = y-1; m <= y+1; m++){
-                alive_neighbours += out_of_map_rows ? OUTSIDE_STATE : get_cell_state(n,m);
+                out_of_map_cols = (m == -1) || (m == get_world_width());
+                alive_neighbours += out_of_map_cols ? OUTSIDE_STATE : get_cell_state(n, m);
             }
         }
         else {
@@ -150,7 +170,7 @@ void initialize_world(void){
 }
 
 void load_default_world(void){
-    int temp_world[][4] = {{0, 0, 0, 0}, {1, 0, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+    int temp_world[][4] = {{0, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
     for(int i = 0; i < WORLD_HEIGHT; i++) {
         for(int j = 0; j < WORLD_WIDTH; j++) {
             world[i][j] = temp_world[i][j];
@@ -159,7 +179,7 @@ void load_default_world(void){
 }
 
 int get_cell_state(int x, int y) {
-    return world[x-1][y-1];
+    return world[x][y];
 }
 
 int get_world_height(void) {
@@ -172,7 +192,7 @@ int get_world_width(void) {
 
 void print_world(void) {
     int row, columns;
-    for (row=0; row <= get_world_height(); row++){
+    for (row=0; row < get_world_height(); row++){
         for(columns=0; columns < get_world_width(); columns++){
             printf("%d  ", world[row][columns]);
         }
