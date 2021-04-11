da07a47ff4dfa3444c37c36ee9117e0bda822e2d
juan-sb <sbruzzi.juan@outlook.com>
2021-04-11 16:33:16 -0300

    Merge lógica con punteros + lo que había hecho

--- 5-tp/11.c
+++ 5-tp/11.c
@@ -1,3 +1,4 @@
+
 // Implementar un programa que reciba del usuario dos palabras y
 // determine si la segunda de ellas puede formarse completamente con letras
 // contenidas en la primera. Por ejemplo, morsa/rosa, japoneses/esponja.
@@ -14,30 +15,44 @@
 
 #include <stdio.h>
 
+// La palabra mas larga de la lengua espanola es electroencefalografista,
+// que tiene 23 caracteres
+#define MAX_LENGTH 23
+
 // Constant definitions
 #define 🤌 ((ch = getchar()) != '\n' && ch != EOF) && ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) && (i <= 23)
 #define LEN_ERROR 10
 #define CHAR_ERROR 20
 
 // Function prototypes
-void strInput(char str[]);
+int str_input(char *);
+void flush_stdin(char);
 void print_str(void);
 int contar_len(char arr[]);
 int sanitize_input(char arr[], int);
 
-
-char palabra1[24] = "cammila"; 
-char palabra2[24] = "mama";
+char palabra1[MAX_LENGTH + 1] = "cammila"; 
+char palabra2[MAX_LENGTH + 1] = "mama";
 int cont = 0;
 
 int main(void) {
+  // Input
+  printf("Ingrese una palabra: ");
+  str_input(palabra1);
+  // Acá habría que validarla
+
+  printf("Ingrese otra palabra: ");
+  str_input(palabra2);
+  // Acá habría que validarla
+
+   
+
+  // Lo que había
+
 
   print_str();
   int len1 = contar_len(palabra1);
   int len2 = contar_len(palabra2);
-
-  
-
   int irasig = 0;
 
   // Iterates over each char in palabra2 and looks for an unused match in palabra1
@@ -51,7 +66,6 @@ int main(void) {
           }
       }
   }
-
   // Prints the outcome of the program
   if(cont == len2 - 1){
     printf("si, %d, %d\n", cont, len2);                                 // TODAVIA HAY QUE DEJAR LINDO ESTO
@@ -59,6 +73,45 @@ int main(void) {
   else{
     printf("no, %d, %d\n", cont, len2);
   }
+//      printf("LEN_ERROR: the string entered exceeds the 23 character limit.\n");
+//      printf("There are both length and character errors in your entry. See: %c\n", arr[error_at]);
+
+
+
+
+
+  // Lo de Juan
+
+
+  char *pp1 = palabra1;
+  char *pp2 = palabra2;
+  int letras_encontradas = 0;
+  int flag_letra_encontrada = 0;
+  int letras_p2 = 0;
+  while(*pp2){
+    letras_p2++;
+
+    flag_letra_encontrada = 0;
+    
+    pp1 = palabra1;
+    while(*pp1 && !flag_letra_encontrada) {
+      //printf("Comparando %c con %c\n", *(pp1), *(pp2));
+      if(*pp1 == *pp2){
+        flag_letra_encontrada = 1;
+        letras_encontradas++;
+        *pp1 = '_';
+      }
+      pp1++;
+    }
+    //printf("\n");
+    pp2++;
+  }
+  if(letras_encontradas == letras_p2){
+    printf("Nice!");
+  } else {
+    printf("No nice¡ :c");
+  }
+
 }
 
 // Prints out the two input strings
@@ -78,7 +131,7 @@ void print_str(void){
 }
 
 // Counts the length of each input string
-int str_len(char arr[]){
+int contar_len(char arr[]){
   int i = 0;
   int len = 0;
   while (arr[i++] != '\0'){
@@ -87,52 +140,49 @@ int str_len(char arr[]){
   return len;
 }
 
-void strInput(char str[]) {
-  int i = 0;
-  int ch;
-  while(🤌) {
-    str[i++] = ch;
-  }
-  str[i] = '\0';
-  
+
+void flush_stdin(char limit) {
+    char c;
+    // Hasta que se llegue al caracter especificado, vacía stdin
+    while((c=getchar()) != limit) {}
 }
 
-int sanitize_input(char arr[], int len){
-  int len_error = 0;
+int str_input(char *str) {
+  char ch;
+  char *p = str;
+
+  int length_error = 0;
+
+  while(!length_error && ((ch = getchar()) != '\n')) {
+    *(p++) = ch;
+    length_error = (str - p) == (MAX_LENGTH + 1);
+  }
+  if(length_error){
+    return LEN_ERROR;
+  }
+
+  *p = '\0';
+  return 0;
+}
+
+
+void validate_word(char arr[]) {
+  int i = 0;
   int character_error = 0;
   int error_at = 0;
-  if (len > 24){
-    len_error = LEN_ERROR;
-  }
-  else {
-    len_error = 0;
-  }
 
-  for (int i=0; i<=len; i++) {
-    if (((arr[i] >= 'a') && (arr[i] <= 'z') || ((arr[i] >= 'A') && (arr[i] <= 'Z'))) || (arr[i] == '\n')){
-      character_error = 0;
-    }
-    else{
+  while (arr[i++]) {
+    int is_lowcase_letter = (arr[i] >= 'a') && (arr[i] <= 'z');
+    int is_upper_letter = (arr[i] >= 'A') && (arr[i] <= 'Z');
+    
+    if (!(is_lowcase_letter || is_upper_letter || arr[i] == '\n')){
       character_error = CHAR_ERROR;
       error_at = i;
-      break;
-    }
+      break; 
+    }   
   }
 
-  int error_code = character_error + len_error;
-
-  switch (error_code) {
-    case (LEN_ERROR): {
-      printf("LEN_ERROR: the string entered exceeds the 23 character limit.\n");
-      break;
-    }
-    case (CHAR_ERROR): {
-      printf("CHAR_ERROR: there is an invalid character in your input. See: %c\n", arr[error_at]);
-      break;
-    }
-    case (CHAR_ERROR + LEN_ERROR): {
-      printf("There are both length and character errors in your entry. See: %c\n", arr[error_at]);
-      break;
-    }
+  if (character_error == CHAR_ERROR) {
+    printf("CHAR_ERROR: there is an invalid character in your input. See: %c\n", arr[error_at]);
   }
 }
