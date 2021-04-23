d8e18d1034e64adca90d86604fd335dce9736ba1
Mariano <moms@itba.edu.ar>
2021-04-23 07:10:23 -0300

    ejercicio 5

--- 6-tp/omsmarian/5.c
+++ 6-tp/omsmarian/5.c
@@ -1,66 +1,75 @@
 #include <stdio.h>
 
-#define space ' '
+#define space 32
 
 char toUpper (char);                    //recibe letra minus la transforma en Mayus
 char toLower (char);                    //recibe letra Mayus la transforma en minus
 int isUpper (char);                     //chequea que sea Mayus
 int isLower (char);                     //chequea que sea minus
+void normalizar (char *str);
 void CamelCase (char *str);
 
 void main (void)
 {
-    char frase[] ="bETa tEStiG...";
-    int i = (sizeof(frase)/sizeof(frase[0]));
+    char frase[] ="bETa tEStiG... mono tEta";
     printf("Original Frase:\n");
     for(int j = 0; (frase[j]) != '\0'; j++)
     {
         printf("%c", frase[j]);    
     }
     printf("\nCheta Frase\n");
+    normalizar (frase);    
     CamelCase(frase);
+    for(int p = 0; (frase[p]) != '\0'; p++)
+    {
+        printf("%c", frase[p]);    
+    }
+    printf("\n");
 }
-
 
 void CamelCase (char *str)
 {
-    if(isUpper(*str))
+    int space_flag = 0;
+    *str = toUpper(*str);
+    str++;
+    for(str ;(*str)!= '\0'; str++)
     {
-        printf("%c", *str);
-        str++;
-    }
-    else if(isLower(*str))
-    {
-        printf("%c", toUpper(*str));
-        str++;
-    }
-    for(str ; *str!='\0'; str++)
-    {
-        if (*str == space)
+        if((*str) == space)
         {
-            printf("%c", *str);
-            str++;
+            space_flag = 1;
+        }
+        else if(space_flag)
+        {
+            *str= toUpper(*str);
+            space_flag = 0;
+        }
+    }    
+}
+
+void normalizar (char *str)
+{
+    for(str; (*str)!= '\0'; str++)
+    {
+        if ((((*str)>= 'A') && ((*str)<='Z')) || (((*str)>= 'a') && ((*str)<='z')))
+        {
             if(isUpper(*str))
-            {
-                printf("%c", *str);
-            }
-            else if(isLower(*str))
-            {
-                printf("%c",toUpper(*str));
-            }
+            *str = toLower(*str);
+        }
+        else if ((*str) != ((((*str)>= 'A') && ((*str)<='Z')) || (((*str)>= 'a') && ((*str)<='z'))))
+        {
+            *str = *str;
         }
     }
-
 }
 
 char toUpper (char letter)
 {
-    return (letter + ('a'-'A'));
+    return (letter - ('a'-'A'));
 }
 
 char toLower (char letter)
 {
-    return (letter - ('a'-'A'));
+    return (letter + ('a'-'A'));
 }
 
 int isUpper (char letter)
