88419b54231ac50cb8aed7764974c1ac17899fa3
Agustin Fisher <afisher@itba.edu.ar>
2021-05-25 21:38:06 -0300

    a

--- 10-tp/6/main.c
+++ 10-tp/6/main.c
@@ -8,10 +8,15 @@
 #define CHAR_PORTA_CLR  'c'
 #define CHAR_PORTA_TOG  't'
 #define CHAR_EXIT       'q'
-#define CHAR_LED_ON     "🛑"
-#define CHAR_LED_OFF    "  "
 #define IS_ASCII_NUM(c) ((c) >= '0' || (c) <= '7')
 #define ASCII_NUM_INT(c) ((c) - '0')
+
+#if (EMOJI == 1)
+#define CHAR_LED_ON     "🛑"
+#else
+#define CHAR_LED_ON     "00"
+#endif
+#define CHAR_LED_OFF    "  "
 
 void flush_stdin(char limit);
 void dec2bin(unsigned int dec);
@@ -20,31 +25,33 @@ void clear_screen(void);
 
 int main() {
     char c;
-    //un total y completo afano de conway
+    
     clear_screen();
     print_porta_data();
     while ((c = getchar()) != 'q'){
 
-        // Temporary output
         if (c == CHAR_PORTA_TOG) {
+            //switches the state of all PORTA pins
             mask_toggle(PORTA, MSK_BYTE);
         }
-        else if (c == CHAR_PORTA_CLR) {
-            mask_off(PORTA, MSK_BYTE);
-        }
         else if (c == CHAR_PORTA_SET) {
+            //turns on all pins when the 'set' letter is entered
             mask_on(PORTA, MSK_BYTE);
+        }
+        else if (c == CHAR_PORTA_CLR) {
+            //turns off all pins when the 'clear' letter is entered
+            mask_off(PORTA, MSK_BYTE);
         }
         else if (c == CHAR_EXIT) {
             break;
         }
         else if(IS_ASCII_NUM(c)) {
+            //sets the entered bit number to ON
             bit_set(PORTA, ASCII_NUM_INT(c));
         }
         else {
             printf("Sorry, I didn't recognize that input.\n");
         }
-        //flush_stdin('\n');
         if(c != '\n') {
             clear_screen();
             print_porta_data();
@@ -54,12 +61,23 @@ int main() {
     return 0;
 }
 
+/**
+ * @brief Clears stdin until the limit char is found.
+ * 
+ * @param limit char to clear until
+ */
 void flush_stdin(char limit) {
     char c;
-    // Hasta que se llegue al caracter especificado, vacía stdin
     while((c=getchar()) != limit) {}
 }
 
+/**
+ * @brief Converts from decimal to binary using
+ * the LED_ON and LED_OFF characters and prints it 
+ * with purely aesthetic separators.
+ *
+ * @param dec decimal number to be converted
+ */
 void dec2bin(unsigned int dec) {
     int i;
     for(i = 7; i >= 0; i--) {
@@ -70,10 +88,18 @@ void dec2bin(unsigned int dec) {
     }
 }
 
+/**
+ * @brief Clears the terminal
+ * 
+ */
 void clear_screen(void) {
     system("CLS || clear");
 }
 
+/**
+ * @brief Prints port A data to stdin, this is the main
+ * interface for the program
+ */
 void print_porta_data() {
     printf("\tPORTA data: \n\n");
     printf("\t   7   6    5    4    3    2    1    0  \n");
